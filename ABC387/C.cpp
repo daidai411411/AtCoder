e363457fd782cc15511ce4cd28dbf3271bcca506
daidai411411 <daidai411411@gmail.com>
2025-02-07 15:39:49 +0900

    ABC387-c

--- ABC387/C.cpp
+++ ABC387/C.cpp
@@ -26,19 +26,8 @@ int main() {
 	for (int i = 0; i < l.size(); i++) l[i] = l_str[i] - '0';
 	for (int i = 0; i < r.size(); i++) r[i] = r_str[i] - '0';
 
-	ll count = 0;
-	for (ll digits = l.size(); digits <= r.size(); digits++) {
-		for (ll head = (digits == l.size() ? l[0] : 0); head <= (digits == r.size() ? r[0] : 9); head++) {
-			count += pow(head, digits - 1);
-		}
-	}
 
-	ll digits = l.size(), head = l[0];
-	for (int digit = 1; digit <= digits; digit++) {
-        cout -= () * pow(head, digits - digit);
-	}
 
-	cout << count << endl;
 
 	return 0;
 }
